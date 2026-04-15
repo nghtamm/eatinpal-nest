26abd5d0a8e9cd10ca76e535b64f534e09ab00f6
nghtamm <tachibanakanade2003@gmail.com>
2026-04-15 16:14:27 +0700

    feat: implement timeout and rate limiter

---
+++ src/common/interceptors/timeout.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  RequestTimeoutException,
+} from '@nestjs/common';
+import { Observable, throwError, TimeoutError } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  constructor(private readonly ms: number = 10000) {}
+
+  intercept(ctx: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      timeout(this.ms),
+      catchError((err) => {
+        if (err instanceof TimeoutError) {
+          return throwError(
+            () => new RequestTimeoutException('Request timed out!'),
+          );
+        }
+        return throwError(() => err);
+      }),
+    );
+  }
+}
